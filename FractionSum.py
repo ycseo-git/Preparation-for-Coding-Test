3eee4971798a1dfc958fedfc3ee12b8998e4cf07
ycseo-git <shirley6188@gmail.com>
2024-03-05 22:04:43 +0900

    분수의 덧셈 : 다양한 풀이

--- FractionSum.py
+++ FractionSum.py
@@ -1,6 +1,10 @@
 from fractions import Fraction
+import math
 
-def solution(numer1, denom1, numer2, denom2):
+
+
+##### My first solution with using module 'fraction' #####
+def solution_1(numer1, denom1, numer2, denom2):
     
     answer_numer = numer1 * denom2 + numer2 * denom1
     answer_denom = denom1 * denom2
@@ -8,3 +12,30 @@ def solution(numer1, denom1, numer2, denom2):
     answer = Fraction(answer_numer, answer_denom)
     
     return [answer.numerator, answer.denominator]
+
+
+
+##### The optimized first solution with using module 'fraction' #####
+def solution_1_2(numer1, denom1, numer2, denom2):
+
+    answer = Fraction(numer1, denom1) + Fraction(numer2, denom2)
+
+    return [answer.numerator, answer.denominator]
+
+
+
+##### The second solution with using module 'math' and its gcd() #####
+def solution_2(numer1, denom1, numer2, denom2):
+    
+    answer_numer = numer1 * denom2 + numer2 * denom1
+    answer_denom = denom1 * denom2
+    gcd = math.gcd(answer_numer, answer_denom)
+    
+    return [answer_numer // gcd, answer_denom // gcd]
+
+
+
+##### For the test #####
+print(solution_1(1,2,3,4))
+print(solution_1_2(1,2,3,4))
+print(solution_2(1,2,3,4))
